9055279ff97d692eeb6448d3ebce38c7e47e6718
Adam Nguyen <adamdnguyen10@gmail.com>
2024-01-05 01:46:37 -0800

    fix: base bath

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/fun-hub/',
   test: {
     globals: true,
     environment: 'jsdom',
