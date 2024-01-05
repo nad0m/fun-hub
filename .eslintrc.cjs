5b4fb64eb09982583feb532fab9a9598d3f39035
Adam Nguyen <adamdnguyen10@gmail.com>
2024-01-05 01:09:40 -0800

    chore: scaffolding

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,6 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'consistent-return': 'off',
   },
 };
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,19 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Lobby } from 'boardgame.io/react';
+import { TicTacToe } from 'atom-games';
+import { TicTacToeBoard } from '@games/tic-tac-toe';
+import { Welcome } from '@components/Welcome/Welcome';
+import { ColorSchemeToggle } from '@components/ColorSchemeToggle/ColorSchemeToggle';
 
 export function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Lobby
+        gameServer={import.meta.env.VITE_ATOM_SOCKET_API}
+        lobbyServer={import.meta.env.VITE_ATOM_SOCKET_API}
+        gameComponents={[{ game: TicTacToe, board: TicTacToeBoard }]}
+      />
     </>
   );
 }
