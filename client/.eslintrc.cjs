79c70ad5f9f09f2b3cc98fc61959b19792b7e1b4
Frank Valdez <fvaldez421@gmail.com>
2024-01-26 11:05:38 -0800

    create monorepo with client

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
+  workingDirectories: [{ mode: 'auto' }],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'consistent-return': 'off',
