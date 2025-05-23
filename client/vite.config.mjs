8e9c47378aae73bed3a18d24fd621505fbabae46
Adam Nguyen <adamdnguyen10@gmail.com>
2025-05-23 12:47:58 -0700

    fix base route

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: process.env.NODE_ENV === 'production' ? '/fun-hub/' : '/',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), tsconfigPaths()],
+    base: '/',
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.mjs',
+    },
+  };
+
+  if (command !== 'serve') {
+    config.base = '/fun-hub/';
+  }
+  return config;
 });
