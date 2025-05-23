6c1f9b59f2305f3787c764faaa5d802190973537
Adam Nguyen <adamdnguyen10@gmail.com>
2025-05-23 10:52:32 -0700

    check env

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: process.env.NODE_ENV === 'production' ? '/fun-hub/' : '/',
   test: {
     globals: true,
     environment: 'jsdom',
